43f69b1cff9cac6d1a20b56f8bfa8edca4be556a
Caio Balduino <caiobalduinodev@hotmail.com>
2024-10-21 18:31:23 -0300

    💫 feat: convert and format currency

--- scripts.js
+++ scripts.js
@@ -8,6 +8,8 @@ const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 amount.addEventListener("input", () => {
   const hasCaractersRegex = /\D+/g;
@@ -32,8 +34,26 @@ form.onsubmit = (e) => {
 
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    let total = amount * price;
+
+    if (isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.");
+    }
+
+    total = formatCurrencyBRL(total).replace("R$", "");
+    result.textContent = `${total} Reais`;
+
     footer.classList.add("show-result");
   } catch (error) {
     footer.classList.remove("show-result");
   }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
